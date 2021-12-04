4bc590772260a354b3a90f4a8ec4ef6e7cb9268d
onebuddingnerd <avikrk8@DESKTOP-LTGSJNM.localdomain>
2021-12-04 15:39:59 -0500

    commented callc

--- rnnFwd.cu
+++ rnnFwd.cu
@@ -1,10 +1,13 @@
 
 #include <cmath>
 #include <math.h>
+#include "CycleTimer.h"
 
 // A is a by b
 // B is b by c
 // AB is a by c
+
+__device__
 void matmul (float* A, float* B, float* AB, int a, int b, int c) {
 
     for (int i = 0; i < a; i++) {
@@ -19,17 +22,20 @@ void matmul (float* A, float* B, float* AB, int a, int b, int c) {
     
 }
 
+__device__
 void getExcerpt (float* A, float* dest, int timestep, int dimsize) {
     memcpy(dest, &A[timestep*dimsize], dimsize*sizeof(float));
     return;
 }
 
+__device__
 void setExcerpt (float* A, float* src, int timestep, int dimsize) {
     for (int i = 0; i < dimsize; i++) {
         A[timestep * dimsize + i] = src[i];
     }
 }
 
+__device__
 void vectorAdd (float* A, float* B, float* AplusB, int a, int b) {
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++) {
@@ -38,20 +44,22 @@ void vectorAdd (float* A, float* B, float* AplusB, int a, int b) {
     }
 }
 
+__device__
 void vectorTanh (float* A, float* ret, int a, int b) {
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++) {
-            ret[i] = std::tanh(A[i*a + j])
+            ret[i] = std::tanh(A[i*a + j]);
         }
     }
 }
 
+__device__
 void vectorSoftmax(float* src, float* dest, int a, int b) {
     float denom = 0;
     
     //PROBLEM if b is not 1 here
     
-    for (int = 0; i < a; i++) {
+    for (int i = 0; i < a; i++) {
         denom += exp(src[i]);
     }
 
@@ -64,8 +72,10 @@ void vectorSoftmax(float* src, float* dest, int a, int b) {
 
 __global__
 void kernelComputeForward (float* device_x, float* device_a, float* device_h, float* device_o, 
-                            float* device_y, int hsize, int vsize, float* W, float* U, float* V,
-                            float* c) {
+                            float* device_y, float* U, float* V, float* W, float* b, float* c,
+                            float* h_tminus1, float* W_h, float* x_t, float* U_x, float* add1,
+                            float* a_t, float* h_t, float* V_h, float* o_t, float* y_t,
+                            int hsize, int vsize) {
     int index = blockIdx.x * blockDim.x + threadIdx.x; // time index
 
     if (index > 0) {
@@ -84,19 +94,19 @@ void kernelComputeForward (float* device_x, float* device_a, float* device_h, fl
     //                  W_h_term + U_x_term
 
     // 1a. the W_h term
-    float* h_tminus1 = calloc(hsize, sizeof(float));
+    // float* h_tminus1 = (float*) calloc(hsize, sizeof(float));
     getExcerpt(device_h, h_tminus1, index-1, hsize);    
-    float* W_h = calloc(hsize, sizeof(float));
+    // float* W_h = (float*) calloc(hsize, sizeof(float));
     matmul(W, h_tminus1, W_h, hsize, hsize, 1);
     // 1b. the ux term
-    float* x_t = calloc(vsize, sizeof(float));
+    // float* x_t = (float*) calloc(vsize, sizeof(float));
     getExcerpt(device_x, x_t, index, vsize);
-    float* U_x = calloc(hsize, sizeof(float));
+    // float* U_x = (float*) calloc(hsize, sizeof(float));
     matmul(U, x_t, U_x, hsize, vsize, 1);
     // 1: addition of b
-    float* add1 = calloc(hsize, sizeof(float));
+    // float* add1 = (float*) calloc(hsize, sizeof(float));
     vectorAdd(U_x, b, add1, hsize, 1);
-    float* a_t = calloc(vsize, sizeof(float));
+    // float* a_t = (float*) calloc(vsize, sizeof(float));
     vectorAdd(add1, W_h, a_t, hsize, 1);
 
     free(h_tminus1);
@@ -108,25 +118,25 @@ void kernelComputeForward (float* device_x, float* device_a, float* device_h, fl
 
     // a_t has the result for the next layer (h)
     // 2: tanh of vector for a_t
-    float* h_t = calloc(hsize, sizeof(float));
+    // float* h_t = (float*) calloc(hsize, sizeof(float));
     vectorTanh(a_t, h_t, hsize, 1);
     setExcerpt(device_h, h_t, index, hsize);
     // free(h_t) LATER
 
     // h_t has the result for the next layer(o)
     // 3: addition of V*h and c
-    float* V_h = calloc(vsize, sizeof(float));
+    // float* V_h = (float*) calloc(vsize, sizeof(float));
     matmul(V, h_t, V_h, vsize, hsize, 1);
-    float* o_t = calloc(hsize, sizeof(float));
+    // float* o_t = (float*) calloc(hsize, sizeof(float));
     vectorAdd(c, V_h, o_t, hsize, 1);
     setExcerpt(device_o, o_t, index, hsize);
     // free(o_t) LATER
 
     // o_t has the result for the next layer(o)
     // 4: softmax(o_t)
-    float* y_t = calloc(vsize, sizeof(float));
+    // float* y_t = (float*) calloc(vsize, sizeof(float));
     vectorSoftmax(o_t, y_t, vsize, 1);
-    setExcerpt(device_y, y_y, index, vsize);
+    setExcerpt(device_y, y_t, index, vsize);
     
     free(a_t);
     free(h_t);
@@ -138,21 +148,35 @@ void kernelComputeForward (float* device_x, float* device_a, float* device_h, fl
 
 
 void forwardPass (float* device_x, float* device_a, float* device_h, float* device_o, 
-                  float* device_y, int vsize, int hsize, int T) {
+                  float* device_y, float* U, float* V, float* W, float* b, float* c,
+                  int hsize, int vsize, int T) {
     const int threadsPerBlock = 512;
     const int blocks = (T + threadsPerBlock - 1) / threadsPerBlock;
 
+    // intermediate terms
+    float* h_tminus1 = (float*) calloc(hsize, sizeof(float));
+    float* W_h = (float*) calloc(hsize, sizeof(float));
+    float* x_t = (float*) calloc(vsize, sizeof(float));
+    float* U_x = (float*) calloc(hsize, sizeof(float));
+    float* add1 = (float*) calloc(hsize, sizeof(float));
+    float* a_t = (float*) calloc(vsize, sizeof(float));
+    float* h_t = (float*) calloc(hsize, sizeof(float));
+    float* V_h = (float*) calloc(vsize, sizeof(float));
+    float* o_t = (float*) calloc(hsize, sizeof(float));
+    float* y_t = (float*) calloc(vsize, sizeof(float));
+
     kernelComputeForward<<< blocks, threadsPerBlock >>>(device_x, device_a, device_h, 
-                                                        device_o, device_y);
+                                                        device_o, device_y, U, V, W, b, c, 
+                                                        h_tminus1, W_h, x_t, U_x,
+                                                        add1, a_t, h_t, V_h, o_t, y_t,
+                                                        hsize, vsize);
 }
 
-double cudaForwardPassTimer (float* x, float* y, float* W, float* V, float* W,
-                              int vsize, int hsize, int T) {
+double cudaForwardPassTimer (float* x, float* y, float* U_host, float* V_host, float* W_host,
+                              float* b_host, float* c_host, int vsize, int hsize, int T) {
     float* device_x;
-    float* device_y;
-    
-    cudaMalloc((void **)&device_x, sizeof(int) * length);
-    cudaMemcpy(device_x, x, length, cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&device_x, sizeof(float) * vsize * T);
+    cudaMemcpy(device_x, x, vsize * T * sizeof(float), cudaMemcpyHostToDevice);
 
     /* 
       shapes:
@@ -172,16 +196,39 @@ double cudaForwardPassTimer (float* x, float* y, float* W, float* V, float* W,
 
     */
 
-    cudaMalloc((void**) &device_x, sizeof(int) * vsize * T);
-    cudaMalloc((void**) &device_a, sizeof(int) * hsize * T);
-    cudaMalloc((void**) &device_h, sizeof(int) * hsize * T);
-    cudaMalloc((void**) &device_o, sizeof(int) * vsize * T);
-    cudaMalloc((void**) &device_y, sizeof(int) * vsize * T);
+    // alloc result (and intermediate result) destinations
+    float* device_a;
+    float* device_h;
+    float* device_o;
+    float* device_y;
+    cudaMalloc((void**) &device_a, sizeof(float) * hsize * T);
+    cudaMalloc((void**) &device_h, sizeof(float) * hsize * T);
+    cudaMalloc((void**) &device_o, sizeof(float) * vsize * T);
+    cudaMalloc((void**) &device_y, sizeof(float) * vsize * T);
+
+    // declare, alloc, and copy onto device the init paramters
+    float* b;
+    cudaMalloc((void**) &b, hsize * sizeof(float));
+    cudaMemcpy(b, b_host, hsize * sizeof(float), cudaMemcpyHostToDevice);
+    float* c;
+    cudaMalloc((void**) &c, vsize * sizeof(float));
+    cudaMemcpy(c, c_host, vsize * sizeof(float), cudaMemcpyHostToDevice);
+    float* U;
+    cudaMalloc((void**) &U, hsize * vsize * sizeof(float));
+    cudaMemcpy(U, U_host, hsize * vsize * sizeof(float), cudaMemcpyHostToDevice);
+    float* W;
+    cudaMalloc((void**) &W, hsize * hsize * sizeof(float));
+    cudaMemcpy(W, W_host, hsize * hsize * sizeof(float), cudaMemcpyHostToDevice);
+    float* V;
+    cudaMalloc((void**) &V, vsize * hsize * sizeof(float));
+    cudaMemcpy(V, V_host, vsize * hsize * sizeof(float), cudaMemcpyHostToDevice);
 
     double startTime = CycleTimer::currentSeconds();
-    forwardPass(device_x, devica_a, device_h, device_o, device_y, 
-                          vsize, hsize, T);
+    forwardPass(device_x, device_a, device_h, device_o, device_y,
+                U, V, W, b, c,
+                vsize, hsize, T);
     cudaThreadSynchronize();
+    cudaMemcpy(y, device_y, vsize * T * sizeof(float), cudaMemcpyDeviceToHost);
     double endTime = CycleTimer::currentSeconds();
     double overallDuration = endTime - startTime;
     
