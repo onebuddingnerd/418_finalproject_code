2e3fce13429c90ac74b57b7f203fa47b25cdf4d5
onebuddingnerd <avikrk8@DESKTOP-LTGSJNM.localdomain>
2021-12-07 12:09:45 -0500

    discovered wrong alloc amt for fowardPass vars while coding h_shared loop (rnnFwd.cu:250)

--- cuda_code/main.cpp
+++ cuda_code/main.cpp
@@ -4,11 +4,15 @@
 #include <string>
 #include <cstring>
 
+#define TIMESTEPS 5
+#define HSIZE 30
+#define VSIZE 8000
+
 double cudaForwardPassTimer (float* x, float* y, float* U_host, float* V_host, float* W_host,
-                              float* b_host, float* c_host, int vsize, int hsize, int T);
+                              float* b_host, float* c_host);
 
 double cudaSequentialForwardPassTimer (float* x, float* y, float* U_host, float* V_host, float* W_host,
-                              float* b_host, float* c_host, int vsize, int hsize, int T);
+                              float* b_host, float* c_host);
 
 void supply_rand_val (float* addr) {
     float ret = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
@@ -23,68 +27,68 @@ int max (int a, int b) {
 }
 
 void init_param_values (float* U, float* V, float* W, float* b,
-                    float* c, int hsize, int vsize) {
+                    float* c) {
     
-    for (int i1 = 0; i1 < max(hsize, vsize); i1++) {
+    for (int i1 = 0; i1 < max(HSIZE, VSIZE); i1++) {
 
-        // fprintf(stdout, "%d of %d outerloop: begun\n", i1, max(hsize, vsize));
+        // fprintf(stdout, "%d of %d outerloop: begun\n", i1, max(HSIZE, VSIZE));
         // fflush(stdout);
 
-        for (int i2 = 0; i2 < max(hsize, vsize); i2++) {
-            if (i1 < hsize && i2 < vsize) {
-                supply_rand_val(&U[i1*vsize + i2]);
+        for (int i2 = 0; i2 < max(HSIZE, VSIZE); i2++) {
+            if (i1 < HSIZE && i2 < VSIZE) {
+                supply_rand_val(&U[i1*VSIZE + i2]);
             }
-            if (i1 < vsize && i2 < hsize) {
-                supply_rand_val(&V[i1*hsize + i2]);
+            if (i1 < VSIZE && i2 < HSIZE) {
+                supply_rand_val(&V[i1*HSIZE + i2]);
             }
-            if (i1 < hsize && i2 < hsize) {
-                supply_rand_val(&W[i1*hsize + i2]);
+            if (i1 < HSIZE && i2 < HSIZE) {
+                supply_rand_val(&W[i1*HSIZE + i2]);
             }
         }
 
-        // fprintf(stdout, "%d of %d outerloop: completed innerloop\n", i1, max(hsize, vsize));
+        // fprintf(stdout, "%d of %d outerloop: completed innerloop\n", i1, max(HSIZE, VSIZE));
         // fflush(stdout);
 
-        if (i1 < hsize) {
+        if (i1 < HSIZE) {
             supply_rand_val(&b[i1]);
         }
-        if (i1 < vsize) {
+        if (i1 < VSIZE) {
             supply_rand_val(&c[i1]);
         }
     }
 
 }
 
-void init_input_values (float* x, int vsize, int timesteps) {
+void init_input_values (float* x) {
 
-    for (int i = 0; i < vsize * timesteps; i++) {
+    for (int i = 0; i < VSIZE * TIMESTEPS; i++) {
         supply_rand_val(&x[i]);
     }
 
 }
 
-float test (int hsize, int vsize, int timesteps, int use_sequential) {
+float test (int use_sequential) {
 
-    float* x = (float*) calloc(vsize * timesteps, sizeof(float));
-    float* y = (float*) calloc(vsize * timesteps, sizeof(float));
-    float* U = (float*) calloc(hsize * vsize, sizeof(float));
-    float* V = (float*) calloc(vsize * hsize, sizeof(float));
-    float* W = (float*) calloc(hsize * hsize, sizeof(float));
-    float* b = (float*) calloc(hsize, sizeof(float));
-    float* c = (float*) calloc(vsize, sizeof(float));
+    float* x = (float*) calloc(VSIZE * TIMESTEPS, sizeof(float));
+    float* y = (float*) calloc(VSIZE * TIMESTEPS, sizeof(float));
+    float* U = (float*) calloc(HSIZE * VSIZE, sizeof(float));
+    float* V = (float*) calloc(VSIZE * HSIZE, sizeof(float));
+    float* W = (float*) calloc(HSIZE * HSIZE, sizeof(float));
+    float* b = (float*) calloc(HSIZE, sizeof(float));
+    float* c = (float*) calloc(VSIZE, sizeof(float));
 
-    init_param_values(U, V, W, b, c, hsize, vsize); // random generation of values that
+    init_param_values(U, V, W, b, c); // random generation of values that
                                                     // could plausibly be computed during training
 
-    init_input_values(x, vsize, timesteps);
+    init_input_values(x)
     
     float duration  = 0;
     if (use_sequential) {
         duration = cudaSequentialForwardPassTimer (x, y, U, V, W, b, c, 
-                                                vsize, hsize, timesteps);
+                                                VSIZE, HSIZE, TIMESTEPS);
     } else { 
         duration = cudaForwardPassTimer (x, y, U, V, W, b, c, 
-                                                vsize, hsize, timesteps);
+                                                VSIZE, HSIZE, TIMESTEPS);
     }
 
     free(x);
@@ -100,19 +104,15 @@ float test (int hsize, int vsize, int timesteps, int use_sequential) {
 
 int main() {
 
-    int hsize = 30;
-    int vsize = 8000; // change for testing; these are typical
-    int timesteps = 5;
-
     fprintf(stdout, "beginning test ... \n");
     fflush(stdout);
-    float duration = test(hsize, vsize, timesteps, 0);
+    float duration = test(0);
     fprintf(stdout, "result duration (parallel): %f\n", duration);
     fflush(stdout);
 
     fprintf(stdout, "beginning test ... \n");
     fflush(stdout);
-    duration = test(hsize, vsize, timesteps, 1);
+    duration = test(1);
     fprintf(stdout, "result duration (sequential): %f\n", duration);
     fflush(stdout);
 
